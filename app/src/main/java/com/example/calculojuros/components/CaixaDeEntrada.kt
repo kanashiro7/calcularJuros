822ab5378a4740cc60115ac4e665181d09779036
Gustavo Kanashiro <gustavo.kanaa@gmail.com>
2024-03-19 22:28:45 -0300

    fetch: state hoisting

---
+++ app/src/main/java/com/example/calculojuros/components/CaixaDeEntrada.kt
@@ -0,0 +1,37 @@
+package com.example.calculojuros.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CaixaDeEntrada(
+    value: String,
+    placeholder: String,
+    label: String,
+    modifier: Modifier,
+    keyboardType: KeyboardType,
+    atualizarValor: (String) -> Unit
+){
+    OutlinedTextField(
+        value = value,
+        onValueChange = {atualizarValor(it)
+                        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        placeholder = {
+            Text(text = placeholder)
+        },
+        label = {
+            Text(text = label)
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType
+        ))
+}
